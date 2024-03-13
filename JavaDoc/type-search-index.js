31a93edb8ef71e12bcc4e9d8afcfef19582f99eb
WWFYOcelot <servals4life@gmail.com>
2024-03-13 13:52:52 -0400

    Added javadoc to gh-pages

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.googletest","l":"CustomMapImage"},{"p":"com.example.googletest","l":"HelloApplication"},{"p":"com.example.googletest","l":"HelloController"},{"p":"com.example.googletest","l":"PotholeDataParser"},{"p":"com.example.googletest","l":"PotholeReader"}];updateSearchResults();
